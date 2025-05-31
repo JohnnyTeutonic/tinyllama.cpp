36944d339428e7a8b0e6fde3332c5a97855fe3c7
JohnnyTeutonic <jonathanreich100@gmail.com>
2025-05-31 21:20:18 +1000

    almost there 3

--- model.h
+++ model.h
@@ -10,12 +10,17 @@
 
 #include "safetensors_loader.h"
 #ifdef HAS_CUDA
-// On Windows, prevent problematic fp16 headers that require missing nv/target in CUDA 12.x
 #ifdef _WIN32
+#ifndef CUDA_NO_HALF
 #define CUDA_NO_HALF
+#endif
 #define __CUDA_NO_HALF_OPERATORS__
 #define __CUDA_NO_HALF_CONVERSIONS__
 #define __CUDA_NO_HALF2_OPERATORS__
+#define __CUDA_NO_BFLOAT16_CONVERSIONS__
+#define CUDA_NO_HALF_OPERATORS
+#define CUDA_NO_HALF_CONVERSIONS
+#define __CUDA_NO_BFLOAT16_CONVERSIONS__
 #endif
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
